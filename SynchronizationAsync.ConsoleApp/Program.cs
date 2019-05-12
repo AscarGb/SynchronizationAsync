73814eac63db81d26195b9f79360763cca3c8f1a
AscarGb <ascargb>
2019-05-13 01:54:47 +0500

    Добавьте файлы проекта.

---
+++ SynchronizationAsync.ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SynchronizationAsync.ConsoleApp
+{
+    class Program
+    {
+        static ManualResetEventAsync mre = new ManualResetEventAsync(false);
+        static void Main(string[] args)
+        {
+            Go();
+            Console.ReadLine();
+        }
+
+        static async void Go()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var k = i;
+                Task.Run(async () =>
+                {
+                    if (!mre.State)
+                        await mre.WaitOneAsync();
+                    Console.WriteLine($"Task id:{k}");
+                });
+            }
+
+            await Task.Delay(new TimeSpan(0, 0, 5));
+
+            mre.Set();
+
+            mre.Reset();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var k = i;
+                Task.Run(async () =>
+                {
+                    if (!mre.State)
+                        await mre.WaitOneAsync();
+
+                    Console.WriteLine($"Task id:{k}");
+
+                });
+            }
+
+            await Task.Delay(new TimeSpan(0, 0, 1));
+
+            mre.Set();
+        }
+    }
+}
---
+++ SynchronizationAsync/ManualResetEventAsync.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationAsync
+{
+    public class ManualResetEventAsync
+    {
+        ConcurrentQueue<ManualResetEventAwaitable> awaiteQueue
+            = new ConcurrentQueue<ManualResetEventAwaitable>();
+
+        public bool State { get; private set; }
+
+        public ManualResetEventAsync(bool initialState)
+        {
+            State = initialState;
+        }
+
+        public ManualResetEventAwaitable WaitOneAsync()
+        {
+            var awaitable = new ManualResetEventAwaitable();
+
+            if (State)
+                awaitable.Set();
+            else
+                awaiteQueue.Enqueue(awaitable);
+
+            return awaitable;
+        }
+
+        public void Set()
+        {
+            lock (awaiteQueue)
+            {
+                while (awaiteQueue.TryDequeue(out var awaitable))
+                    awaitable.Set();
+
+                State = true;
+            }
+        }
+
+        public void Reset()
+        {
+            State = false;
+        }
+    }
+
+    public class ManualResetEventAwaitable
+    {
+        ManualResetEventAwaiter _manualResetEventAwaiter;
+        public ManualResetEventAwaitable()
+        {
+            _manualResetEventAwaiter = new ManualResetEventAwaiter();
+        }
+        public ManualResetEventAwaiter GetAwaiter()
+        {
+            return _manualResetEventAwaiter;
+        }
+        public void Set()
+        {
+            _manualResetEventAwaiter.Set();
+        }
+    }
+
+    public class ManualResetEventAwaiter : INotifyCompletion
+    {
+        Action _continuation;
+        public void OnCompleted(Action continuation)
+        {
+            Volatile.Write(ref _continuation, continuation);
+        }
+
+        public bool GetResult()
+        {
+            return true;
+        }
+
+        public bool IsCompleted { get; private set; } = false;
+
+        public void Set()
+        {
+            IsCompleted = true;
+
+            Action continuation = Interlocked.Exchange(ref _continuation, null);
+
+            Task.Run(() =>
+            {
+                continuation?.Invoke();
+            });
+        }
+    }
+}
